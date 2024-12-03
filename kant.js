72d23e09e850b5762d9583971004366d7b8439ae
Kante <esemeudo372@gmail.com>
2024-12-03 14:36:58 +0000

    ljkhjust changed ua2

--- kant.js
+++ kant.js
@@ -1,16 +1,16 @@
-const un1 = 'Kant';
-let udob1 = '20th aprill';
-const ue1 = 'esmeudo';
-const ua1 = '18'
-console.log(un1,udob1,ue1,ua1);
+// const un1 = 'Kant';
+// let udob1 = '20th aprill';
+// const ue1 = 'esmeudo';
+// const ua1 = '18'
+// console.log(un1,udob1,ue1,ua1);
 
-const un2 = 'Kante';
-let udob2 = '20th march';
-const ue2 = 'esme';
+// const un2 = 'Kante';
+// let udob2 = '20th march';
+// const ue2 = 'esme';
 
 
-let ua2 ='25'
-console.log(un2,udob2,ue2,ua2)
-ua2 = '26'
-console.log(ua2)
+// let ua2 ='25'
+// console.log(un2,udob2,ue2,ua2)
+// ua2 = '26'
+// console.log(ua2)
 
